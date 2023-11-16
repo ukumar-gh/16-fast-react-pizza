9f6067568588d32a9c0ff644410bcd61e5d8601d
Kumar MM <public.kumar@gmail.com>
2023-11-16 12:00:40 -0800

    flex grid

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-  return <div className="text-sm font-semibold">JONAS</div>;
+  return <div className="hidden text-sm font-semibold md:block">JONAS</div>;
 }
 
 export default UserName;
