64b95635201109e1d601bfc9b73d7324257b94ad
Kumar MM <public.kumar@gmail.com>
2023-11-16 20:06:18 -0800

    layer @apply

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -47,13 +47,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -61,21 +61,14 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="w-full rounded-full border border-stone-200 px-4 py-2 
-              text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring
-              focus:ring-yellow-400 md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
             className="h-6 w-6 accent-yellow-400
-            focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:ring-offset-2"
+            focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
