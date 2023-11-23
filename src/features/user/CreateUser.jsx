685b46f9f91a0cfb612d665a63d83d97cd41aeda
Kumar MM <public.kumar@gmail.com>
2023-11-23 10:05:47 -0800

    set up user slice and use username across app

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState(
+    useSelector((state) => state.user.username),
+  );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return null;
+
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
