598c8decb21115f865a0d670b356c71b00636881
Kumar MM <public.kumar@gmail.com>
2023-11-24 12:00:04 -0800

    delete items from cart and menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { addItem } from '../cart/cartSlice';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const itemQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = itemQuantity > 0;
 
   function handleAddToCart() {
     const newItem = {
@@ -36,7 +39,8 @@ function MenuItem({ pizza }) {
           ) : (
             <p className="upper text-sm font-medium text-stone-500">Sold out</p>
           )}
-          {!soldOut && (
+          {isInCart && <DeleteItem pizzaId={id} />}
+          {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
